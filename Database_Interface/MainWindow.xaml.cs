4a105dd9b47fda73c6cb363a443a9cb1bf83fe63
MinistrUtoky <dulov-vitya@mail.ru>
2022-09-27 20:24:01 +0300

    2

---
+++ Database_Interface/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Database_Interface.classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database_Interface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            clsDB.FillDataGrid(tableGrid);
+        }
+
+        private void DB_Update_Add_Record(string sName, string sDescription)
+        { 
+            string sSQL = "SELECT TOP 1 * FROM tbl_gay WHERE [name] Like '" + sName + "'";
+            DataTable tbl = clsDB.Get_DataTable(sSQL);
+            if (tbl.Rows.Count == 0)
+            {
+                string sql_Add = "INSERT INTO tbl_gay ([name],[description]) VALUES('" + sName + "','" + sDescription + "')";
+                clsDB.Execute_SQL(sql_Add);
+            }
+            else
+            {
+                string ID = tbl.Rows[0]["id"].ToString();
+                string sql_Update = "UPDATE tbl_gay SET [name] = \"GAY\" WHERE id = " + ID;
+                clsDB.Execute_SQL(sql_Update);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DB_Update_Add_Record("gay", "really gay");
+            clsDB.FillDataGrid(tableGrid);
+        }
+    }
+}
---
+++ Database_Interface/classes/clsDB.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Controls;
+
+namespace Database_Interface.classes
+{
+    public static class clsDB
+    {
+
+        public static void FillDataGrid(DataGrid tableGrid)
+        {
+
+            string cn_String = Properties.Settings.Default.connectionString;
+            using (SqlConnection con = new SqlConnection(cn_String))
+            {
+                string cmdString = "SELECT id, name, description FROM tbl_gay";
+                SqlCommand cmd = new SqlCommand(cmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("tbl_gay");
+                sda.Fill(dt);
+                tableGrid.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        public static SqlConnection Get_DB_Connection()
+        {
+            string cn_String = Properties.Settings.Default.connectionString;
+            SqlConnection cn_connection = new SqlConnection(cn_String);
+            if (cn_connection.State != ConnectionState.Open) cn_connection.Open();
+            return cn_connection;
+        }
+
+
+
+        public static DataTable Get_DataTable(string SQL_Text)
+        {
+            SqlConnection cn_connection = Get_DB_Connection();
+            DataTable table = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(SQL_Text, cn_connection);
+            adapter.Fill(table);
+            return table;
+        }
+
+
+
+        public static void Execute_SQL(string SQL_Text)
+        {
+            SqlConnection cn_connection = Get_DB_Connection();
+            SqlCommand cmd_Command = new SqlCommand(SQL_Text, cn_connection);
+            cmd_Command.ExecuteNonQuery();
+        }
+
+        public static void Close_DB_Connection()
+        {
+            string cn_String = Properties.Settings.Default.connectionString;
+            SqlConnection cn_connection = new SqlConnection(cn_String);
+            if (cn_connection.State != ConnectionState.Closed) cn_connection.Close();
+        }
+    }
+}
